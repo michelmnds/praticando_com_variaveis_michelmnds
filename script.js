ff83a0cea5aa8e49987afb46e27b790373dcee54
Michel <m1tchofc@gmail.com>
2022-10-11 19:13:25 +0100

    versão final

---
+++ script.js
@@ -0,0 +1,74 @@
+/*let valorComrpa = parseFloat(prompt("Qual o valor da compra?"))
+let valorLitro = 6.80
+let litros = valorComrpa / valorLitro
+
+alert(`O Cliente abasteceu ${litros}l.`)*/
+
+
+/*let valorCelsius = parseFloat(prompt("Insira um valor em ºc"))
+let valorFahrenheit = (9 * valorCelsius + 160) / 5
+
+alert(`O valor ${valorCelsius} em Fahrenheit é ${valorFahrenheit}`)*/
+
+
+/*let valorFahrenheit = parseFloat(prompt("Insira um valor em Fahrenheit"))
+let valorCelsius = ((valorFahrenheit - 32)*5)/9
+
+alert(`O valor ${valorFahrenheit} em Celsius é ${valorCelsius}`)*/
+
+
+/*let comprimento = parseInt(prompt("Insira um comprimento"))
+let altura = parseInt(prompt("Insira uma altura"))
+let largura = parseInt(prompt("Insira uma altura"))
+
+let volume = comprimento * altura * largura
+
+alert(`O volume é de: ${volume}m³⁠`)*/
+
+
+/*let valorInteiro = parseInt(prompt("Insira um número inteiro"))
+let valorInteiroAoQuadrado = valorInteiro*valorInteiro
+
+alert(`O valor ${valorInteiro} ao quadrado é ${valorInteiroAoQuadrado}`)*/
+
+
+/*let x1 = parseInt(prompt("Insira uma nota"))
+let x2 = parseInt(prompt("Insira otura nota"))
+let x3 = parseInt(prompt("Insira otura nota"))
+let x4 = parseInt(prompt("Insira otura nota"))
+let x5 = parseInt(prompt("Insira otura nota"))
+
+let somaNotas = x1 + x2 + x3 + x4 + x5
+let mediaNotas = (x1 + x2 + x3 + x4 + x5)/5
+
+alert(`A soma dos valores informados é de: ${somaNotas} e a média das notas é: ${mediaNotas}`)*/
+
+
+/*let inteiro1 = parseInt(prompt("Insira um valor inteiro"))
+let inteiro2 = parseInt(prompt("Insira outro valor inteiro"))
+
+let divisao = inteiro1 % inteiro2
+
+alert(`O resto da divisão de ${inteiro1} por ${inteiro2} é: ${divisao}`)*/
+
+
+/*let texto = prompt("Insira uma frase ou palavra")
+let cumprimento = texto.length
+
+alert(`Esse texto tem ${cumprimento} caracteres.`)*/
+
+
+/*let texto = prompt("Insira uma frase ou palavra")
+let primeiraLetra = texto.charAt(0).toUpperCase(texto)
+let ultimaLetra = texto.slice(-1).toLowerCase(texto)
+
+alert(`A primeira letra deste texto é ${primeiraLetra} e última é o ${ultimaLetra}`)*/
+
+
+
+/*let nome = prompt("Insira seu nome") 
+let sombrenome = prompt("Insira seu sobrenome") 
+let idade = parseInt(prompt("insira sua idade"))
+let setor = prompt("Insira seu setor") 
+
+alert(`nome: ${nome.toUpperCase(nome 0)} ${sombrenome} - idade: ${idade} - Setor: ${setor}`)*/
